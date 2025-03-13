371c08476c5b460f389d094987110bda1c94a809
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-03-13 00:23:37 +0000

    [FEAT]: auth layout and starter setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React, { ReactNode } from "react";
+import { RocketIcon } from "lucide-react";
+import dynamic from "next/dynamic";
+
+// Dynamically import Lottie to prevent SSR issues
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+import authAnimation from "@/public/animations/auth-animation.json";
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <main className="auth-container">
+      {/* Authentication Form Section */}
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-col md:flex-row gap-3 items-center justify-center">
+            <RocketIcon size={50} className="text-blue-500" />
+            <div>
+              <h1 className="text-3xl md:text-5xl font-bold">Startup Pub</h1>
+              <p className="text-gray-300 text-sm md:text-base">
+                Discover and support startups in your area.
+              </p>
+            </div>
+          </div>
+
+          <div className="">{children}</div>
+        </div>
+      </section>
+
+      {/* Animation Section */}
+      <section className="auth-illustration flex justify-center items-center mt-3">
+        <div className="w-3/4 md:w-full max-w-lg">
+          <Lottie animationData={authAnimation} loop={true} />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default Layout;
