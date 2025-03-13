9adbc2e7dd6f87440cd026f312e82377c223a9be
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-03-13 01:10:58 +0000

    [UPDATE]: ui for the forms are complete

--- components/app-components/auth-components/AuthForm.tsx
+++ components/app-components/auth-components/AuthForm.tsx
@@ -1,16 +1,40 @@
 "use client";
-import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import React from "react";
 import {
   DefaultValues,
   FieldValues,
+  Path,
   SubmitHandler,
   useForm,
   UseFormReturn,
 } from "react-hook-form";
 import { z, ZodType } from "zod";
+
+const FIELD_NAMES = {
+  fullName: "Full Name",
+  email: "Email",
+  password: "Password",
+  confirmPassword: "Confirm Password",
+};
+
+const FIELD_TYPES = {
+  fullname: "text",
+  email: "email",
+  password: "password",
+  confirmPassword: "password",
+};
 
 interface Props<T extends FieldValues> {
   type: "SIGN_UP" | "SIGN_IN";
@@ -38,10 +62,34 @@ function AuthForm<T extends FieldValues>({
         {type === "SIGN_UP" ? "Sign Up" : "Sign In"}
       </h1>
       <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(handleSubmit)}
-          className="space-y-8"
-        ></form>
+        <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+          {Object.keys(defaultValues).map((field) => (
+            <FormField
+              key={field}
+              control={form.control}
+              name={field as Path<T>}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="capitalize">
+                    {FIELD_NAMES[field.name as keyof typeof FIELD_NAMES]}
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      required
+                      type={FIELD_TYPES[field.name as keyof typeof FIELD_TYPES]}
+                      {...field}
+                      className="form-input"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          ))}
+
+          <Button type="submit" className="form-btn">
+            {type === "SIGN_UP" ? "Sign Up" : "Sign In"}
+          </Button>
+        </form>
       </Form>
       <p className="text-center text-base font-medium">
         {type === "SIGN_UP"
