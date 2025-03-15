93215547d6dfd2aaa970c65bd890dd12ac1a9f63
EmmanuelKeifala <emmanuelkeifala@gmail.com>
2025-03-15 09:26:28 +0000

    [FIX]: video not playing issue

--- components/app-components/StartUpVideo.tsx
+++ components/app-components/StartUpVideo.tsx
@@ -4,13 +4,24 @@ import { IKVideo, ImageKitProvider } from "imagekitio-next";
 import React from "react";
 
 function StartUpVideo({ videoUrl }: { videoUrl: string }) {
+  const vidPath = videoUrl.split("https://ik.imagekit.io/startuppubdir")[1];
+
   return (
-    <ImageKitProvider
-      publicKey={config.imagekit.publicKey}
-      urlEndpoint={config.imagekit.urlEndpoint}
-    >
-      <IKVideo path={videoUrl} controls={true} className="w-full rounded-xl" />
-    </ImageKitProvider>
+    <div className="flex justify-center items-center w-full">
+      <div className="w-full max-w-3xl aspect-video">
+        <ImageKitProvider urlEndpoint={config.imagekit.urlEndpoint}>
+          <IKVideo
+            path={vidPath}
+            controls={true}
+            className="w-full h-full rounded-xl object-cover shadow-lg"
+            style={{
+              minHeight: "360px",
+              maxHeight: "540px",
+            }}
+          />
+        </ImageKitProvider>
+      </div>
+    </div>
   );
 }
 
